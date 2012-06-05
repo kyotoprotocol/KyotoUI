a8a58e48289c4eca3d067acc306e35d12010fa40
Jonathan Webster <webbo89@gmail.com>
2012-06-06 00:41:25 +0100

    Added new files

---
+++ JettyMavenWarApp/src/main/java/org/example/JettyMavenWarApp/AnalyseServlet.java
@@ -0,0 +1,23 @@
+package org.example.JettyMavenWarApp;
+ 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+public class AnalyseServlet extends HttpServlet
+{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Analyse Servlet</h1>");
+        response.getWriter().println("session=" + request.getSession(true).getId());
+    }
+}
