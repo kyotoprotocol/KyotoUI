c2323508c0933bf13a84786ebacb609d78e5f417
Jonathan Webster <webbo89@gmail.com>
2012-06-06 17:41:54 +0100

    csv

--- initialise.php
+++ initialise.php
@@ -11,11 +11,25 @@ $m = new Mongo();
     $list = $db->listCollections();
     foreach ($list as $collection) {
         echo "8==> $collection... <br>";
-     //   $collection->drop();
-     //   echo "gone\n";
     }
     
+    
     $db->createCollection("CountryDefaults");
+    $CountryDefaults = $db->selectCollection("CountryDefaults");
+    
+    $file = fopen('admin/data.csv', 'r');
+    // grab the first line - for headers possibly useful later
+     $line = fgetcsv($file);
+    while (($line = fgetcsv($file)) !== FALSE) {
+        
+        //add each country as a new object in mongo
+        $country = array ("ISO" => $line[2], "name" => $line[1], "type" => $line[0], "totalArea" => $line[5], "landArea" => $line[6], "waterArea"  => $line[7], "arableLand" => $line[8]);
+        $CountryDefaults->insert($country);
+
+    }
+    fclose($file);
+
+ 
     
 } catch (MongoConnectionException $e)
 {
