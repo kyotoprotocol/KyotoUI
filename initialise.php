44680fc0d82f4f59fddcee45c562c6361d13abe6
Jonathan Webster <webbo89@gmail.com>
2012-06-06 17:10:41 +0100

    test creatinga new limitless table

--- initialise.php
+++ initialise.php
@@ -8,6 +8,15 @@ $m = new Mongo();
     $db = $m->selectDB(DB);
     #echo 'david';
     
+    $list = $db->listCollections();
+    foreach ($list as $collection) {
+        echo "> $collection... <br>";
+     //   $collection->drop();
+     //   echo "gone\n";
+    }
+    
+    $db->createCollection("CountryDefaults");
+    
 } catch (MongoConnectionException $e)
 {
     echo $e;
