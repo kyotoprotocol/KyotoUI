4d13f0007a3c7d1b87501048d1d8a8a656740655
Max Revitt <max.revitt@gmail.com>
2012-06-06 18:34:29 +0100

    jquery bootstrap.js index contains a dropdown for you lee

--- head.php
+++ head.php
@@ -1,6 +1,9 @@
 <html>
 
     <LINK REL=StyleSheet HREF="css/bootstrap.css" TYPE="text/css">    
+    <script src="http://code.jquery.com/jquery-1.7.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+    
     <h1> Kyoto Interface </h1>  
     
 <div class="navbar navbar-fixed-top">
@@ -8,12 +11,23 @@
       <a class="brand" href="#">
         Kyoto Simulator
       </a>
-    <ul class="nav">
-    <li class="active">
-        <a href="index.php">Home</a>
+      
+<ul class="nav nav-pills">
+    <li class="dropdown" id="menu1">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#menu1">
+            Pre-Simulation
+            <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+        <li><a href="initialise.php"">Initialise</a></li>
+        <li><a href="#">Another action</a></li>
+        <li><a href="#">Something else here</a></li>
+        <li class="divider"></li>
+        <li><a href="#">Separated link</a></li>
+        </ul>
     </li>
-    <li><a href="initialise.php">Initialise</a></li>
-    </ul>
+</ul>
+
   </div>
 </div>
     
--- index.php
+++ index.php
@@ -4,4 +4,19 @@ include('head.php');
 
 
 echo 'index page';    
+
 ?>
+
+<br>
+
+<ul class="nav nav-pills">
+  <li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="path/to/page.html">
+      Dropdown
+      <b class="caret"></b>
+    </a>
+    <ul class="dropdown-menu">
+      ...
+    </ul>
+  </li>
+</ul>
