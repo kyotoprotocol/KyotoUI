84f4d316dff31048ea9b1a128122ca6eca915a91
webbo89 <webbo89@gmail.com>
2012-06-07 15:33:42 +0100

    david

--- initialise.php
+++ initialise.php
@@ -28,7 +28,6 @@ try {
     // grab the first line - for headers possibly useful later
      $line = fgetcsv($file);
     while (($line = fgetcsv($file)) !== FALSE) {
-        
         //add each country as a new object in mongo
         $country = array ("ISO" => $line[2], "name" => $line[1], "type" => $line[0], "totalArea" => $line[5], "landArea" => $line[6], "waterArea"  => $line[7], "arableLand" => $line[8], "ISO2" => $line[9]);
         $CountryDefaults->insert($country);
