d01daa6cde9080009f4235cc12692bf7ea6b2a37
Max Revitt <max.revitt@gmail.com>
2012-06-08 02:26:56 +0100

    nearly there

--- country.php
+++ country.php
@@ -13,48 +13,39 @@ include('admin/config.php');
     
         // Load specific simulation
         if (isset($_GET['simid'])) {
-            $sim = $simsDB->findOne(array("_id" => $_GET['simid']));
-            var_dump($simsDB->findOne(array("_id" => (int)$_GET['simid'], 'countries.ISO' => (string)$_GET['country']), array('countries' => 1)));
+            $sim = $simsDB->findOne(array("_id" => (int)$_GET['simid']));
+            if(isset($_GET['country'])){
+                $country = $sim['countries'][$_GET['country']];
+            } else {
+                $country = $sim['countries']['ALB'];
+            } 
         } else {
             $sim = $simsDB->findOne();
-            var_dump($simsDB->findOne(array("_id" => (int)$sim['_id'], 'countries.ISO' => (string)$_GET['country']), array('countries' => 1)));
+            if(isset($_GET['country'])){
+                $country = $sim['countries'][$_GET['country']];
+            } else {
+                $country = $sim['countries']['ALB'];
+            }  
         }
     
-        foreach($sim['countries'] as $country){
-            $dropdown[] = ($country['ISO'] . '     ' . $country['name']);
+        foreach(array_keys($sim['countries']) as $key){
+            $cDrop[] = array('ISO' => $key, 'name' => $sim['countries'][$key]['name']);
         }
         
-    //die();
-    /*
         //Grab updated country data - SORT LATER
             if (isset($_POST['_id'])){                       //check post data set
             foreach(array_keys($_POST) as $key){
-                $countryData[$key] = $_POST[$key];
+                //$country = $sim['countries'][$_POST[$key]];
             }
-            $mongoId = new MongoID($_POST['_id']);          //ensure proper Mongo ID
-            $CountryDefaults->update(array('_id' => $mongoId), $countryData);
-            $smarty->assign('updated', true);
-        }  */
-    
-        
-        // Load specific country from within simulation
-        if (isset($_GET['country'])) {
-            
-            //var_dump(array_search($_GET['country'], $sim['countries']));
-            //$country = $sim['countries'][array_search($_GET['country'], $sim['countries'])];
-        } else {
-            $country = $sim['countries'][0];
+            //$mongoId = new MongoID($_POST['_id']);          //ensure proper Mongo ID
+            //$CountryDefaults->update(array('_id' => $mongoId), $countryData);
+            //$smarty->assign('updated', true);
         }
         
-        //var_dump($country);
-        die();
-        
         $smarty->assign('country', $country);
-        $smarty->assign('dropdown',$dropdown);                    
+        $smarty->assign('cDrop',$cDrop);                    
         $smarty->display('views/country.tpl');
 
-        $cursor = $countryDefaults->find();
- 
 } catch (MongoConnectionException $e) {
     echo $e;
 }
--- views/country.tpl
+++ views/country.tpl
@@ -1,5 +1,5 @@
 {extends file="views/layout.tpl"}
-{block name=title}Country View {$country}{/block}
+{block name=title}Country View - {$country['name']}{/block}
 {block name=head}
   <script type="text/javascript" src="http://www.google.com/jsapi"></script>
 <script type="text/javascript">
@@ -9,7 +9,7 @@
     function drawVisualization() {
         var data = google.visualization.arrayToDataTable([
             ['Country', 'Selected'],
-            ['{$country}', 1]
+            ['{$country['name']}', 1]
         ]);
 
         var geomap = new google.visualization.GeoMap(
@@ -31,19 +31,19 @@
 
 {if ($updated)}
     <div class="alert alert-success">
-        <strong>Success!</strong> {$country} updated.
+        <strong>Success!</strong> {$country['name']} updated.
     </div>
 {/if} 
 
 <div class="row">
     <div class="span4">
-        <h1>{$country}</h1>
+        <h1>{$country['name']}</h1>
         <ul class="nav nav-pills">
             <li class="dropdown">
                 <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="path/to/page.html">Dropdown<b class="caret"></b></a>
                 <ul class="dropdown-menu">
-                {foreach $dropdown as $c}
-                    <li><a href='country.php?country={$c.ISO}' >{$c.name}</a></li>
+                {foreach $cDrop as $c}
+                    <li><a href='country.php?country={$c['ISO']}' >{$c['name']}</a></li>
                 {/foreach}
 
                 </ul>
@@ -58,7 +58,7 @@
         <form class="well" action="country.php?country={$countrydata['ISO']}" method="post">
         <table class="table table-striped">
              <tbody>
-                {foreach $countrydata as $c}
+                {foreach $country as $c}
                 <tr>
                         <td>{$c@key}</td>
                         <td><input type="text" name="{$c@key}" value="{$c}"></td>
