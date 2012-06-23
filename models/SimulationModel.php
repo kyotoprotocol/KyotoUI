2dc948d15fa7256215f39dbbb0d1fd57fea2fa6d
Jonathan Webster <webbo89@gmail.com>
2012-06-23 18:24:22 +0100

    type fix to try and match presage cli

--- models/SimulationModel.php
+++ models/SimulationModel.php
@@ -6,11 +6,11 @@ class SimulationModel extends BaseMongoRecord
     
     public function beforeSave()
     {
-        $this->setFinishedAt(new MongoInt64($this->getFinishedAt()));
+        $this->setFinishedAt(new MongoInt32($this->getFinishedAt()));
         $this->setCreatedAt(new MongoInt64($this->getCreatedAt()));
         $this->setParent(new MongoInt64($this->getParent()));
         $this->setFinishTime(new MongoInt64($this->getFinishTime()));
-        $this->setCurrentTime(new MongoInt64($this->getCurrentTime()));
+        $this->setCurrentTime(new MongoInt32($this->getCurrentTime()));
     }
 }
 
