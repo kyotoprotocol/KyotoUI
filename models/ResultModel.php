6485360346d0f83006074d9b230459eff54aedea
Max Revitt <max.revitt@gmail.com>
2012-06-24 23:06:35 +0100

    results page is taking some sort of shape

--- models/ResultModel.php
+++ models/ResultModel.php
@@ -3,5 +3,18 @@
 class ResultModel extends BaseMongoRecord 
 {
     protected static $collectionName = 'results';
+    
+    public function beforeSave()
+    {
+        $this->setGDP(new MongoInt64($this->getGDP()));
+        $this->setGDPRate(new MongoInt64($this->getGDPRate()));
+        $this->setArableLandArea(new MongoInt64($this->getArableLandArea()));
+        $this->setAvailableToSpend(new MongoInt64($this->getAvailableToSpend()));
+        $this->setCarbonAbsorption(new MongoInt64($this->getCarbonAbsorption()));
+        $this->setCarbonOffset(new MongoInt64($this->getCarbonOffset()));
+        $this->setCarbonOutput(new MongoInt64($this->getCarbonOutput()));
+        $this->setEmissionsTarget(new MongoInt64($this->getEmissionsTarget()));
+        $this->setEnergyOutput(new MongoInt64($this->getEnergyOutput()));
+    }
 }
 ?>
