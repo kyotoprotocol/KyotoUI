19ce0e69eca6029c51a950889507321572962145
Jonathan Webster <webbo89@gmail.com>
2012-06-24 16:08:36 +0100

    including maps

--- maps/test.php
+++ maps/test.php
@@ -5,8 +5,36 @@
  * and open the template in the editor.
  */
 
+define ("DB", "presage");
+define ("SIMTREE", "simulations");
+define ("LOCAL_HOST", "127.0.0.1:27017");
+include('../libs/mongorecord/BaseMongoRecord.php');
+include('../models/TradeModel.php');
+session_start();
+if(isset($_SESSION['database'])) {
+    if($_SESSION['database']=='remote') {
+        define ("HOST", REMOTE_HOST);
+    } else {
+        $_SESSION['database']='local';
+        define ("HOST", LOCAL_HOST);
+    }
+} else {
+        $_SESSION['database']='local';
+        define ("HOST", LOCAL_HOST);    
+}
+
+try {
+BaseMongoRecord::$connection = new Mongo(HOST);
+BaseMongoRecord::$database = 'presage';
+    } catch(MongoConnectionException $e) {
+        //var_dump($e);
+    }
+
+
+// INCLUDE CONFIG LATER
 //$_GET['direction']
 $country = $_GET['country'];
+$simID = $_GET['simid'];
 
 $filename = "code_to_coordinates.json";
 $handle = fopen($filename, "r");
@@ -45,15 +73,49 @@ function generateFlightPathArray($origin, $remote, $data, $jsoncountries, $names
         
         $array[] = 'M '. (int)$originx . ' ' . (int)$originy . ' Q ' . (int)$width . ' ' .  (int)$height . ' ' . (int)$remotex . ' ' . (int)$remotey . ' ';
         $array[] = $names->$remote;
-        $array[] = $data;
+        $array[] = (string)$data;
     return $array;
 }
 
-$test['USA'] = generateFlightPathArray($country, 'USA', '999999', $countries, $names);
-$test['GRL'] = generateFlightPathArray($country, 'GRL', '999999', $countries, $names);
-$test['AUS'] = generateFlightPathArray($country, 'AUS', '999999', $countries, $names);
+$jsonOut = array();
 
-echo json_encode($test);
+    if ($_GET['direction']=='all') {
+        $tradeQ = new TradeModel();    // instantiate collection model
+        $trades = $tradeQ->find(array('simID'=>$simID));
+        $lines = array();
+        foreach ($trades as $trade){
+            if (isset($lines[$trade->getInitiator()][$trade->getBroadcaster()])){
+                $lines[$trade->getInitiator()][$trade->getBroadcaster()]['value'] += (int)$trade->getQuantity();
+            } else {
+                $lines[$trade->getInitiator()][$trade->getBroadcaster()]['value'] = (int)$trade->getQuantity();
+            }
+        }
+        arsort($lines);
+        foreach ($lines as $key => $value){
+            foreach ($value as $country => $cost) {
+                $jsonOut[$key] = generateFlightPathArray($country, $key, (int)$cost, $countries, $names);
+            }
+        }
+    } else {
+        $tradeQ = new TradeModel();    // instantiate collection model
+        $trades = $tradeQ->find(array('simID'=>$simID,'broadcaster'=>$country));
+        $lines = array();
+        foreach ($trades as $trade){
+            if (isset($lines[$trade->getInitiator()])){
+                $lines[$trade->getInitiator()] += (int)$trade->getQuantity();
+            } else {
+                $lines[$trade->getInitiator()] = (int)$trade->getQuantity();
+            }
+        }
+        arsort($lines);
+        foreach ($lines as $key => $value){
+            $jsonOut[$key] = generateFlightPathArray($country, $key, (int)$value, $countries, $names);
+        }
+    }
+
+//var_dump($jsonOut);
+header('content-type: application/json');
+echo json_encode($jsonOut);
 
 ?>
 
