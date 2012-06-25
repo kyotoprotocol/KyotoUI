780e541883fd380d8df87256442112a098fe975c
Max Revitt <max.revitt@gmail.com>
2012-06-25 16:01:12 +0100

    run button

--- ajax.php
+++ ajax.php
@@ -167,7 +167,7 @@ switch ($_GET['func']) {
         ajaxSend($output);
         break;
     case  'run' :
-        $command = './~/git/kyoto.kyoto/sim-run.sh run '.$sim->getID().' > /var/log/kyoto/log'.$sim->getID().'.txt &';
+        $command = './~/git/kyoto/kyoto/sim-run.sh run '.$sim->getID().' > /var/log/kyoto/log'.$sim->getID().'.txt &';
         exec($command, $output);
         ajaxSend(array($command, $output));
         break;
